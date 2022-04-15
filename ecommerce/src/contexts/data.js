51b1a673497eef8210ce4aec5bce284ba5851720
AugsutoAHP <augustohenrique.pereira@hotmail.com>
2022-04-15 19:26:03 -0300

    Ajustes de produtos padrões

--- ecommerce/src/contexts/data.js
+++ ecommerce/src/contexts/data.js
@@ -1,7 +1,7 @@
 const data = [
   {
     image: 'https://images.kabum.com.br/produtos/fotos/210360/monitor-gamer-aorus-48-oled-120-hz-4k-uhd-1ms-hdmi-displayport-usb-c-freesync-premium-hdr-10-130-srgb-som-integrado-fo48u_1633188554_gg.jpg',
-    title: 'Monitor Gamer Aorus 48 OLED',
+    title: 'Monitor Gamer Aorus 48',
     price: '11.199,90',
     info: '120 Hz, 4K UHD, 1ms, HDMI/DisplayPort/USB-C, FreeSync Premium, HDR 10, 130% sRGB, Som Integrado - FO48U',
     weight: '2,9',
@@ -22,17 +22,17 @@ const data = [
   },
   {
     image: 'https://images.kabum.com.br/produtos/fotos/266992/ifood-digital-g-brazil-_1636597587_gg.jpg',
-    title: 'Cartão de Presente: iFood - R$50,00',
+    title: 'Gift Card iFood-R$50,00',
     price: '45,00',
     info: 'O iFood Card é um cartão presente que dá a liberdade do comprador ou presenteado escolher as delícias que mais gosta no nosso aplicativo',
-    weight: 'Produto Digital',
+    weight: '0',
   },
   {
     image: 'https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_LeagueofLegends_RiotGames_S2_1200x1600-7fd64f0f7b674900bdd172967865d545',
     title: 'Conta League of Legends',
     price: '849,90',
     info: 'Conta Mestre - 238 Skins - Todos Campeões Liberados',
-    weight: 'Produto Digital',
+    weight: '0',
   },
   
 ];
