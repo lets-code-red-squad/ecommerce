3ca113394a43e8c2925a358dce8ad5a250a10c87
AugsutoAHP <augustohenrique.pereira@hotmail.com>
2022-04-15 19:12:38 -0300

    Adicionando produtos padrões

--- ecommerce/src/contexts/data.js
+++ ecommerce/src/contexts/data.js
@@ -1,10 +1,17 @@
 const data = [
   {
-    image: 'https://imgs.casasbahia.com.br/1001307454/1xg.jpg',
-    title: 'PlayStation 5',
-    price: '4.099,00',
-    info: 'A Sony agora com tudo o que um gamer sempre sonhou. Um um sistema poderoso e capacidade para rodar os melhores e exclusivos jogos!\nCurta seu ssd de altíssima velocidade, uma imersão mais profunda com suporte a feedback tátil, gatilhos adaptáveis e áudio 3D, além de uma geração inédita de jogos incríveis para PlayStation.',
-    weight: '3,9',
+    image: 'https://images.kabum.com.br/produtos/fotos/210360/monitor-gamer-aorus-48-oled-120-hz-4k-uhd-1ms-hdmi-displayport-usb-c-freesync-premium-hdr-10-130-srgb-som-integrado-fo48u_1633188554_gg.jpg',
+    title: 'Monitor Gamer Aorus 48 OLED',
+    price: '11.199,90',
+    info: '120 Hz, 4K UHD, 1ms, HDMI/DisplayPort/USB-C, FreeSync Premium, HDR 10, 130% sRGB, Som Integrado - FO48U',
+    weight: '2,9',
+  },
+  {
+    image: 'https://imgs.casasbahia.com.br/1508790875/1xg.jpg',
+    title: 'Xbox Series X',
+    price: '5.299,00',
+    info: 'Poder, rapidez e diversão. Essa três palavras traduzem o que o Xbox Series X irá te proporcionar',
+    weight: '4,5',
   },
   {
     image: 'https://imgs.casasbahia.com.br/1001307454/1xg.jpg',
@@ -14,18 +21,18 @@ const data = [
     weight: '3,9',
   },
   {
-    image: 'https://imgs.casasbahia.com.br/1001307454/1xg.jpg',
-    title: 'PlayStation 5',
-    price: '4.099,00',
-    info: 'BLA BLA BLA BLA',
-    weight: '3,9',
+    image: 'https://images.kabum.com.br/produtos/fotos/266992/ifood-digital-g-brazil-_1636597587_gg.jpg',
+    title: 'Cartão de Presente: iFood - R$50,00',
+    price: '45,00',
+    info: 'O iFood Card é um cartão presente que dá a liberdade do comprador ou presenteado escolher as delícias que mais gosta no nosso aplicativo',
+    weight: 'Produto Digital',
   },
   {
-    image: 'https://imgs.casasbahia.com.br/1001307454/1xg.jpg',
-    title: 'PlayStation 5',
-    price: '4.099,00',
-    info: 'A Sony agora com tudo o que um gamer sempre sonhou. Um um sistema poderoso e capacidade para rodar os melhores e exclusivos jogos!\nCurta seu ssd de altíssima velocidade, uma imersão mais profunda com suporte a feedback tátil, gatilhos adaptáveis e áudio 3D, além de uma geração inédita de jogos incríveis para PlayStation.',
-    weight: '3,9',
+    image: 'https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_LeagueofLegends_RiotGames_S2_1200x1600-7fd64f0f7b674900bdd172967865d545',
+    title: 'Conta League of Legends',
+    price: '849,90',
+    info: 'Conta Mestre - 238 Skins - Todos Campeões Liberados',
+    weight: 'Produto Digital',
   },
   
 ];
