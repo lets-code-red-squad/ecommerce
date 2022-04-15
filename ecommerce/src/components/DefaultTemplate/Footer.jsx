4938d3b23dab8a07d09542b054c253cb059412b0
AugsutoAHP <augustohenrique.pereira@hotmail.com>
2022-04-15 20:05:23 -0300

    Adicionado logo no header e footer

--- ecommerce/src/components/DefaultTemplate/Footer.jsx
+++ ecommerce/src/components/DefaultTemplate/Footer.jsx
@@ -1,5 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex">Mussum Ipsum, cacilds vidis litro abertis.</footer>
+    <footer className="text-footer">Let's Shop!® é uma marca registrada de Red Squad Group | CNPJ: 12.345.678/0001-12 | Todos os direitos reservados. Os preços anunciados neste site ou via e-mail promocional podem ser alterados sem prévio aviso. 
+    <br></br>O Let's Shop! não é responsável por erros descritivos. As fotos contidas nesta página são meramente ilustrativas do produto e podem variar de acordo com o fornecedor/lote do fabricante.</footer>
   );
 }
