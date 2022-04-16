a5d96b5048e650a0cdc581e8cf60c2153d4b9635
Bruna Luchini <brunaluchini22@gmail.com>
2022-04-16 00:45:54 -0300

    mudança no css

--- ecommerce/src/components/DefaultTemplate/Footer.jsx
+++ ecommerce/src/components/DefaultTemplate/Footer.jsx
@@ -1,6 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="text-footer">Let's Shop!® é uma marca registrada de Red Squad Group | CNPJ: 12.345.678/0001-12 | Todos os direitos reservados. Os preços anunciados neste site ou via e-mail promocional podem ser alterados sem prévio aviso. 
-    <br></br>O Let's Shop! não é responsável por erros descritivos. As fotos contidas nesta página são meramente ilustrativas do produto e podem variar de acordo com o fornecedor/lote do fabricante.</footer>
+    <footer className="text-footer flex">
+      <p>
+        Let's Shop® é uma marca registrada de Red Squad | CNPJ: 12.345.678/0001-12 | Todos os direitos reservados. Os preços anunciados neste site ou via e-mail promocional podem ser alterados sem prévio aviso.
+      </p>
+      <p>
+        O Let's Shop não é responsável por erros descritivos. As fotos contidas nesta página são meramente ilustrativas do produto e podem variar de acordo com o fornecedor/lote do fabricante.
+      </p>
+    </footer>
   );
 }
