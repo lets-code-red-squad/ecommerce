fe56e34fff1c2d1f2b779a4c5ba20dbea1926fb7
Bruna Luchini <brunaluchini22@gmail.com>
2022-04-18 18:45:26 -0300

    correção de css

--- ecommerce/src/contexts/data.js
+++ ecommerce/src/contexts/data.js
@@ -1,43 +1,552 @@
 const data = [
   {
-    image: '  https://images.kabum.com.br/produtos/fotos/210360/monitor-gamer-aorus-48-oled-120-hz-4k-uhd-1ms-hdmi-displayport-usb-c-freesync-premium-hdr-10-130-srgb-som-integrado-fo48u_1633188554_gg.jpg',
-    title: 'Monitor Gamer Aorus 48',
-    price: 11199.90,
-    info: '120 Hz, 4K UHD, 1ms, HDMI/DisplayPort/USB-C, FreeSync Premium, HDR 10, 130% sRGB, Som Integrado - FO48U',
-    weight: 2.5,
+    image:
+      "https://images.kabum.com.br/produtos/fotos/210360/monitor-gamer-aorus-48-oled-120-hz-4k-uhd-1ms-hdmi-displayport-usb-c-freesync-premium-hdr-10-130-srgb-som-integrado-fo48u_1633188540_g.jpg",
+    title:
+      "Monitor Gamer Aorus 48 OLED, 120 Hz, 4K UHD, 1ms, HDMI DisplayPort USB-C, FreeSync Premium, HDR 10, 130% sRGB, Som Integrado - FO48U",
+    price: 11199.9,
+    info: `Características:
+- Marca: Aorus
+- Modelo: FO48U
+
+Especificações:
+
+Tela:
+- Tamanho do painel (diagonal): 47,53 "OLED
+- Exibir área de visualização (HxV): 1052,16 x 591,84 (mm)
+- Luz de fundo / tipo do painel: OLED
+- Superfície da tela (antirreflexo / reflexo): Anti-reflexo
+- Saturação de cor: 98% DCI-P3 / 130% sRGB
+- Resolução: 3.840 x 2.160 (UHD)
+- Pixel Pitch: 0,274 (H) x 0,274 (V) (mm)
+- Brilho: 130-150 cd / m2 (típico) | 800-900 cd / m2 (@ pico de 3%)
+- Relação de contraste: 135000: 1
+- Ângulo de visão: 178 ° (H) / 178 ° (V)
+- Cores de exibição: 10 bits
+- Tempo de resposta: 1ms GTG
+- Taxa de atualização: 120Hz *
+- HDR: (HDR10 / HLG)
+- Sem cintilação: sim
+- HBR3: sim
+
+Conexões:
+- 2x HDMI 2.1*
+- 1x Display port 1.4 (DSC)
+- 1x USB Type-C
+- 1x Entrada para fone de ouvido
+- 2x USB 3.0
+* HDMI2.1 é compatível com PS5 e Xbox Series X em 4K UHD a 120 Hz.
+
+Áudio:
+- Alto falante: 15 W x 2 + 20 W x 1
+
+Recursos Exclusivos:
+- OSD Sidekick
+- Aim Stabilizer
+- Black Equalizer 1.0
+- Dashboard
+- Timer
+- Counter
+- Crosshair
+- PiP / PbP
+- Atualização automática
+- KVM
+- 6 eixos Controle de cor
+- Espaço Áudio
+
+Alimentação:
+- Tipo de energia: Integrado
+- Entrada CA máx.: 325W
+- Modo de economia de energia: 0,5 W
+- Modo Desligado: 0,3 W
+- Voltagem: AC 100-240V ~ 50 / 60Hz
+
+Estrutura:
+- Montagem na parede VESA: 300 x 300 mm
+- Fechadura Kensington
+
+Dimensões:
+- Com suporte: 1068,5 x 675,1 x 251,1 mm
+- Sem suporte: 1068,5 x 611,8 x 43,1 mm
+
+Conteúdo da Embalagem:
+- Monitor Gamer Aorus
+- Cabo de alimentação
+- Cabo HDMI
+- Cabo DisplayPort
+- Cabo USB
+- QSG
+- Cartão de garantia
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+29641 gramas (bruto com embalagem)`,
+    weight: 2.5
   },
   {
-    image: '  https://imgs.casasbahia.com.br/1508790875/1xg.jpg',
-    title: 'Xbox Series X',
-    price: 5299,
-    info: 'Poder, rapidez e diversão. Essa três palavras traduzem o que o Xbox Series X irá te proporcionar',
-    weight: 4.5,
+    image: "https://imgs.casasbahia.com.br/1508790875/1xg.jpg",
+    title: "Console Xbox Series X 1TB + Controle Sem Fio - Preto",
+    price: 4999,
+    info: `Características:
+
+- Marca: Microsoft
+
+Especificações:
+
+- CPU Zen 2 personalizada de 8X Cores @ 3,8 GHz (3,66 GHz com SMT)
+- GPU RDNA 2 personalizado de 12 TFLOPS, 52 CUs @1.825 GHz SOC Die Size. 360,45 mm
+- Processo. 7 nm aprimorado MEMÓRIA E ARMAZENAMENTO
+- Memória. GDDR6 de 16 GB com barramento de 320 bits Largura de banda de memória. 10 GB a 560 GB/s, 6 GB a 336 GB/s.
+- Armazenamento interno. SSD NVME personalizado de 1TB Taxa de transferência de E/S. 2,4 GB/s (Raw), 4,8 GB/s (compactado, com bloco de descompressão de hardware personalizado)
+- Armazenamento expansível. O suporte para o cartão de expansão Seagate de 1 TB para Xbox Series X|S corresponde exatamente ao armazenamento interno (vendido separadamente).
+- Suporte para HDD externo USB 3.1 (vendido separadamente).
+- Resolução de jogos. Verdadeiro 4K Alto Alcance Dinâmico. Até 8K HDR Unidade óptica. Blu-Ray 4K UHD
+- Meta de desempenho. Até 120 FPS Recursos HDMI. Modo automático de baixa latência. Taxa de atualização variável HDMI. AMD FreeSync.
+- Som Dolby Digital 5.1 DTS 5.1 Dolby TrueHD com Atmos Até 7,1 L-PCM
+- Conectividade: 1 porta HDMI 2.1 USB. 3 portas USB 3.1 Gen 1 Sem fio. Banda dupla 802.11ac Ethernet. 802.3 10/100/1000
+- Opções de acessórios. Rádio sem fio Xbox de banda dupla dedicado.
+
+Conteúdo da Embalagem:
+
+- Xbox Series X 1tb
+
+Garantia do Fornecedor
+
+3 Meses
+
+Peso:
+2000 gramas (bruto com embalagem)`,
+    weight: 4.5
   },
   {
-    image: '  https://imgs.casasbahia.com.br/1001307454/1xg.jpg',
-    title: 'PlayStation 5',
+    image:
+      "https://images.kabum.com.br/produtos/fotos/308350/kit-console-sony-playstation-5-controle-sem-fio-ps5-dualsense_1644939898_g.jpg",
+    title: "Kit Console Sony Playstation 5 + Controle Sem fio PS5 DualSense",
+    price: 4679.9,
+    info: `Características:
+- Marca: Sony
+- Modelo: PlayStation 5
+
+Tipo:
+- Mídia Física
+
+CPU:
+- x86-64-AMD Ryzen “Zen 2”
+- 8 Cores / 16 Threads
+- Frequência variável, até 3.5 GHz
+
+GPU:
+- Mecanismo de gráficos baseado em AMD Radeon RDNA 2
+- Aceleração Ray Tracing
+- Frequência variável, até 2.23 GHz (10.3 TFLOPS)
+
+Memória:
+- Tipo: GDDR6
+- Capacidade: 16GB
+- Largura de banda de 448 GB/s
+
+Armazenamento:
+- Tipo: SSD
+- Capacidade: 825GB
+- Largura de banda de leitura de 5.5GB/s (Raw)
+
+Disco dos Jogos:
+- Tipo: Ultra HD Blu-ray
+- Até 100GB / disco
+
+Saída de Vídeo:
+- Suporte a TVs de 120Hz 4K, TVs 8K, VRR (especificado pelo HDMI versão2.1)
+
+Áudio:
+- “Tempest” 3D AudioTech
+
+Recurso:
+- Compatível com VR (PSVR)
+
+Físico:
+- Cor: Branco
+- Dimensões: 390 x 104 x 260 mm
+
+Tensão de Alimentação:
+- Voltagem: 100-240V 1.65-0.75A 50/60Hz 350W
+
+Entrada / Saída:
+- 1x USB Type-A port (Hi-Speed USB)
+- 2x USB Type-A port (Super-Speed USB 10Gbps)
+- 1x USB Type-C port (Super-Speed USB 10Gbps)
+
+Rede:
+- Ethernet (10BASE-T, 100BASE-TX, 1000BASE-T)
+- IEEE 802.11 a/b/g/n/ac/ax
+- Bluetooth 5.1
+
+Temperatura de Operação:
+- 5º C a 35º C
+
+CONTROLE SEM FIO DUALSENSE
+
+Físico:
+- Cor: Branco
+- Dimensões: 160 x 66 x 106 mm
+
+Botões:
+- Botão PS
+- Botão Criar
+- Botão Options
+- Botões direcionais (Cima/Baixo/Esquerda/Direita)
+- Botões de Ação (Triângulo, Círculo, Cruz, Quadrado)
+- Botões R1/L1, Botões R2/L2 (com efeitos de gatilho)
+- Controle esquerdo / botão L3
+- Controle direito / botão R3
+- Botão do Touch Pad
+- Botão MUTE
+
+Touch Pad:
+- 2 Point Touch Pad
+- Capacitive Type
+- Click Mechanism
+
+Sensor de Movimento:
+- Sistema sensor de movimento com 6 eixos (giroscópio de 3 eixos + acelerômetro de 3 eixos)
+
+Bateria:
+- Tipo: Bateria de íon-lítio recarregável embutida
+- Voltagem: DC 3.65V
+- Capacidade: 1560mAh
+
+Comunicação:
+- Sem Fio: Bluetooth Ver5.1
+- Com Fio: USB connection (HID, Audio)
+
+Resposta:
+- Efeitos de gatilho (nos botões R2/L2)
+- Vibração (resposta tátil com atuador duplo)
+- Indicadores (indicador de luz / indicador de jogador / indicador do MUTE)
+
+Áudio:
+- Microfone embutido
+- Alto-Falante Mono integrado, entrada para Headset Stereo
+- Output : 48kHz/16bit, Input : 24kHz/16bit
+
+Conteúdo da Embalagem:
+- Console PlayStation 5 com SSD de 825GB
+- Controle sem fio DualSense
+- Base
+- Cabo HDMI
+- Cabo de energia AC
+- Cabo USB
+- Materiais impressos (idioma Português)
+- ASTRO’s PLAYROOM (jogo pré-instalado)*
+
+*O console pode precisar ser atualizado para a versão mais recente do software do sistema. Necessário conexão com a Internet.
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+7403 gramas (bruto com embalagem)`,
+    weight: 3.9
+  },
+  {
+    image:
+      "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSyFhZu06X20uyeHp_vkGGpAboOs8LDjZt5fLLn_tqhTUnmfIex-c-zDlmp8hLMzGuKbCSTpjur4dhj34KXmFZhe-Lqooyfzg&usqp=CAY",
+    title: `Cadeira Gamer Husky Gaming Snow, Preto, Cilindro de Gás Classe 4, Base Em metal, Roda Em Nylon - HSN-BK`,
     price: 4099,
-    info: 'A Sony agora com tudo o que um gamer sempre sonhou. Um um sistema poderoso e capacidade para rodar os melhores e exclusivos jogos!\nCurta seu ssd de altíssima velocidade, uma imersão mais profunda com suporte a feedback tátil, gatilhos adaptáveis e áudio 3D, além de uma geração inédita de jogos incríveis para PlayStation.',
-    weight: 3.9,
+    info: `A Cadeira Gamer Husky Gaming Snow proporciona alto conforto e qualidade para as melhores horas do seu dia! Tudo isso aliado a um ótimo custo benefício.
+
+O Melhor Para Seu Game
+
+Os games do momento ficam ainda mais empolgantes ao serem jogados em uma Cadeira Gamer Husky Gaming Snow, pois além do design moderno e robusto, a cadeira proporciona ótima performance nas partidas devido a sensação de conforto que é convertida em foco e concentração, tudo que um gamer de verdade precisa para ser o melhor!
+
+Conforto Para os Gamers
+
+A Gamer Husky Gaming Snow conta com uma estrutura consistente e de fácil montagem, com rodas 100% Nylon, função butterfly que regula a sua altura de forma prática, couro sintético PU, um balanço com até 13º de ajuste e ainda um assento de 510x500mm com encosto de 510 x 700 mm, é fácil perceber que ela foi projetada para entregar muito conforto para os gamers de forma acessível.
+
+Dê um Upgrade no Seu Espaço Gamer
+
+A Husky Gaming Snow conta com espuma de alta qualidade e suas características mais elogiadas são regulagem de altura por pistão à gás e design singular e ao mesmo tempo muito moderno. Dê um upgrade no seu espaço gamer com uma Cadeira Husky Gaming Snow e sinta a diferença! Qualidade com custo benefício para você enfrentar qualquer desafio gamer!`,
+    weight: 3.9
   },
   {
-    image: '  https://images.kabum.com.br/produtos/fotos/266992/ifood-digital-g-brazil-_1636597587_gg.jpg',
-    title: 'Gift Card iFood - R$50,00 ',
-    price: 45,
-    info: 'O iFood Card é um cartão presente que dá a liberdade do comprador ou presenteado escolher as delícias que mais gosta no nosso aplicativo',
-    weight: 0,
+    image:
+      "https://images-americanas.b2w.io/produtos/01/00/img/3081358/8/3081358804_1SZ.jpg",
+    title:
+      "Notebook Samsung Book Intel Celeron 6305, 4GB RAM, HD 500GB, Linux, 15.6 Full HD, Intel UHD Graphics, Cinza Chumbo",
+    price: 4099,
+    info: `Características:
+- Marca: Samsung
+- Modelo: NP550XDZ-KO4BR
+
+Geral:
+- Placa Mãe: Samsung
+- Sistema Operacional: Linux Console (sem GUI)
+
+Processador:
+- CPU: Intel Celeron 6305 (1.80 GHz, 4 MB L3 Cache)
+- Geração: 6ª Geração
+- Chipset: Integrado (Intel)
+
+Memória:
+- Memória Instalada: 4 GB 
+- Tipo de Memória: DDR4 (4 GB x1) 2666MHz
+- Máx. Memória Suportada: 32 GB
+- Slots: 2 slots (1 livre)
+
+Armazenamento:
+- HDD ou SSD ou e.MMC: 500 GB 
+- Rotação do HDD (RPM): 5400 RPM 
+- Slots / Baias: 1 slot m.2 (livre) / 1 baia 2.5" (ocupada) 
+
+Portas de Expansibilidade:
+- Memória e HDD
+
+Tela:
+- Tamanho da Tela (pol): 15.6" Full HD LED antirreflexiva
+- Resolução da Tela: Full HD 1920 x 1080
+
+Placa Gráfica:
+- Processador Gráfico: Intel UHD Graphics
+- Memória Gráfica: Integrada
+
+Recursos Multimídia:
+- Alto-Falantes: 3W Estéreo (1.5W x 2)
+- Microfone Integrado: Sim 
+- Webcam Integrada: Sim
+
+Comunicação:
+- Placa de Rede: Gigabit Ethernet [10/100/1000] 
+- Placa de Rede Wireless: 802.11 ac 1x1 
+- Bluetooth: Bluetooth v5.1
+
+Portas de Comunicação:
+- HDMI: Sim 
+- Saída Fone-de-Ouvido: Sim (Combo com Saída para Fone-de-Ouvido)
+- Entrada p/ Microfone: Sim (Combo com Entrada para Microfone)
+- Leitor de Cartões Multimídia: MicroSD Card Reader 
+- Ethernet RJ45 (LAN): Sim 
+
+Características para Digitação:
+- Teclado: Português-BR / Teclado Númerico Integrado
+- Touchpad: Sim, com suporte à função multi-toques
+
+Segurança:
+- TPM: Sim
+- Slot Trava de Segurança: Slot padrão Kensington
+
+Software: 
+- Softwares Inclusos: Nenhum 
+
+Alimentação de Energia:
+- Fonte Adaptadora: 40W / 60W (Pode variar com o modelo)
+- Tensão da Fonte Adaptadora: Bivolt (100-240V) 
+- Padrão da Bateria: 43 Wh
+
+Dimensões:
+- 35.9 x 24.1 x 1.88 cm
+
+Conteúdo da Embalagem:
+- 1x Computador portátil
+- 1x fonte adaptadora
+- 1x Guias de usuário
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+2900 gramas (bruto com embalagem)`,
+    weight: 3.9
   },
   {
-    image: '  https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_LeagueofLegends_RiotGames_S2_1200x1600-7fd64f0f7b674900bdd172967865d545',
-    title: 'Conta League of Legends',
-    price: 849.9,
-    info: 'Conta Mestre - 238 Skins - Todos Campeões Liberados',
-    weight: 0,
+    image:
+      "https://images.kabum.com.br/produtos/fotos/184924/notebook-acer-aspire-5-intel-core-i5-4gb-ram-ssd-256gb-14-0-hd-windows-10-hsl-prata-a514-53-5239_1628020558_gg.jpg",
+    title:
+      "Notebook Acer Aspire 5 Intel Core i5-1035G1, 4GB RAM, SSD 256GB NVMe, 14 HD Ultrafino, UHD Graphics, Windows 10 Home, Prata - A514-53-5239",
+    price: 3499.9,
+    info: `Características:
+- Marca: Acer
+- Modelo: A514-53-5239
+
+Especificações:
+
+Sistema Operacional:
+- Windows 10 Home 64-Bit
+
+CPU:
+- Intel Core™ i5 – 1035G1
+- Quad Core (8 Threads)
+- Frequência: 1.00 GHz até 3.60 GHz
+- 6MB Intel Smart Cache
+
+Memória:
+- 4 GB RAM DDR4 2400 MHz (4 GB soldado)
+- Expansível até 12GB DDR4 2400 MHz (4GB soldad + Slot de até 8GB)
+
+Tela:
+- 14.0” com design ultrafino
+- Painel: TN 
+- Resolução: HD (1366x768) 
+- Tecnologia antirreflexo Acer ComfyViewTM
+- Frame rate: 60 Hz 
+- Tempo de resposta: 8 ~ 11ms 
+- Brilho: 220 nits 
+- Taxa de contraste: 400:1 
+- Color gamut (NTSC): 45%
+
+Gráfico:
+- Intel UHD Graphics G1 com memória compartilhada com a memória RAM.
+- Suporte às tecnologias OpenGL 4.5, OpenCLTM 2.2 e Microsoft DirectX 12.
+
+Áudio:
+- Dois alto-falantes estéreo
+- Tecnnologia Acer TrueHarmony
+
+Microfone:
+- Duplo om tecnologia Acer Purified Voice
+- Compatível com Cortana com voz
+
+Armazenamento:
+- 256 GB SSD PCIe 3.0 NVMe x4 (M.2 2280)
+
+Upgrade:
+- Este modelo possuí capacidade para a instalação e/ou melhorias de SSD e HDD
+- Slot ocupado M.2 2280, compatível com SSD PCIe 3.0 NVMe x2/x4 de até 512GB (Não acompanha o produto). 
+- Slot livre SATA 3, compatível com HDD ou SSD SATA 3 2.5” de até 2TB (Não acompanha o produto). 
+
+* Para maiores informações ligar para a o número 0800-762-2237
+
+Webcam:
+- Webcam Acer HD com 1280x720 de resolução e Gravação de audio e video em 720p
+
+Wireless e Rede:
+
+Wireless (Wi-Fi):
+- Compatível com IEE 802.11a/b/g/n/ac wireless LAN
+- Suporte a redes com frequência de 2.4 GHz e 5 GHz
+- Com tecnologia MU-MIMO 2x2 
+- Bluetooth 5.0
+
+LAN/Rede com fio:
+- Gigabit Ethernet, Wake-on-LAN ready
+
+Segurança:
+- Leitor biométrico integrado ao touchpad, Senhas para BIOS e HDD, Trava Kensington , Solução TPM em Firware (FTPM)
+
+Alimentação e bateria:
+
+Fonte de alimentação: 
+- Adaptador AC bivolt de 3 pinos (45W) com cabo certificação INMETRO
+
+Bateria: 
+- Bateria de 3 células (Li-Íon) 48Wh 
+- Autonomia da bateria de até 10.5 horas (depende das condições de uso)
+
+Teclado e Touchpad
+
+Teclado: 
+- Tipo membrana em português do Brasil padrão ABNT 2 
+- Atalho com função multimídia (Play/pause, parar, voltar, avançar, aumentar volume e diminuir volume)
+
+Touchpad:
+- Multi-gestual com dois botões
+- Certificação Microsoft Precision Touchpad
+- Leitor biométrico integrado ao touchpad
+
+Produto:
+- Este produto não possuí leitor de CD/DVD 
+- A CPU e GPU são soldadas na máquina
+
+Cor:
+- Prata
+
+Aplicativos
+- Acer Care Center
+- Acer Configuration Manager
+- Quick Access
+- Acer Product Registration
+
+Conteúdo da Embalagem:
+- Notebook Acer aspire 5 
+- Kit para instalação de HDD 
+- Fonte carregadora do notebook
+- Manual em português
+- Termo de garantia
+
+Garantia:
+1 ano de garantia (9 meses de garantia contratual junto ao fabricante + 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor)
+
+Peso:
+2185 gramas (bruto com embalagem)`,
+    weight: 3.9
   },
+  {
+    image:
+      "https://images.kabum.com.br/produtos/fotos/241487/pc-gamer-redragon-arius-amd-rayzen-4700s-rgb-radeon-rx-550-2gb-ddr5-16gb-gddr6-ssd-256gb-windows-10-trial-preto-40628_1634309069_gg.jpg",
+    title:
+      "PC Gamer Redragon Arius AMD Ryzen 4700S, RGB, Radeon RX 550 2GB DDR5, 16GB GDDR6, SSD 256GB, Windows 10 Trial, Preto - 40628",
+    price: 5111,
+    info: `Características:
+
+- Marca: Redragon
+- Modelo: 40628
+
+Especificações:
+
+Gabinete:
+- Redragon Thunder Cracker 
+- Preto com 3 Fans RGB
+- Modelo: GC-605
+
+Fonte:
+- Redragon
+- 500 W
+- 80 PLUS
+- Modelo: RGPS500W
+
+SSD:
+- ADATA 256GB 2.5" SATA III (BULK)
+- Modelo: SU800BR3-25
+
+Placa de Vídeo:
+- AMD RX 550 2GB GDDR5
+- 128-BIT DP+HDMI+DVI-D
+- Modelo: AXRX 550 2GBD5-HLEV2
+
+Placa Mãe:
+- Mini ITX
+
+Processador:
+- AMD 4700S 
+- 8 núcleos 
+- Arquitetura “Zen 2”
+
+Solução térmica:
+- Cooler
+
+RAM:
+- 16GB de memória GDDR6
+
+Conexões:
+- Slots PCIe: 1x slot para PCIe x16, suporte a (sinal x4 de 2ª ger.)
+- Portas SATA: 2
+- Portas: Áudio;,3x USB3 de 2ª Ger. 1x 1ª Ger; 4x USB2, 1 Lan
+
+Cabo de Força:
+- PIXXO 0,75mm
+
+Conteúdo da Embalagem:
+- PC Gamer Redragon Arius AMD Ryzen 4700S
+
+Peso:
+8800 gramas (bruto com embalagem)`,
+    weight: 3.9
+  }
 ];
 
-data.forEach((element) => element['id'] = element.title.toLowerCase().split(' ').join('-'))
-
-console.log(data)
+data.forEach(
+  (element) =>
+    (element["id"] = element.title.toLowerCase().split(" ").join("-"))
+);
 
 export default data;
